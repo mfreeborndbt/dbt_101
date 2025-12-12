322de5d732013030649a035360258c76547620f8
Miles Freeborn <miles.freeborn@dbtlabs.com>
2025-12-12 18:57:54 +0000

    new branch

--- models/marts/nation_rev.sql
+++
@@ -1,9 +0,0 @@
-with nation_revenue as (
-    select
-        nation_name,
-        sum(total_revenue_final) as total_revenue
-    from {{ ref('customer_orders') }}
-    group by nation_name
-)
-
-select * from nation_revenue
